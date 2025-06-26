0322e1b2515066417e7c6c27962e804d87248723
onble <62949816+onble@users.noreply.github.com>
2025-06-26 23:38:05 +0800

    11-脚本的一些变量声明与食物预制体制作

--- src/Head.ts
+++ src/Head.ts
@@ -2,33 +2,26 @@ const { regClass, property } = Laya;
 
 @regClass()
 export class Head extends Laya.Script {
-    //declare owner : Laya.Sprite3D;
-    //declare owner : Laya.Sprite;
+    declare owner: Laya.Sprite;
 
-    @property(String)
-    public text: string = "";
+    @property(Array(Laya.Node))
+    public bodyArray: Array<Laya.Node> = [];
 
-    //组件被激活后执行，此时所有节点和组件均已创建完毕，此方法只执行一次
-    //onAwake(): void {}
+    @property(Laya.Prefab)
+    public bodyPrefab: Laya.Prefab = null;
 
-    //组件被启用后执行，例如节点被添加到舞台后
-    //onEnable(): void {}
+    @property(Laya.Prefab)
+    public foodPrefab: Laya.Prefab = null;
 
-    //组件被禁用时执行，例如从节点从舞台移除后
-    //onDisable(): void {}
+    @property(Number)
+    public bodyNum: number = 2;
 
-    //第一次执行update之前执行，只会执行一次
-    //onStart(): void {}
+    @property(Number)
+    public bodyDistance: number = 50;
 
-    //手动调用节点销毁时执行
-    //onDestroy(): void {}
+    public speed: number = 200;
 
-    //每帧更新时执行，尽量不要在这里写大循环逻辑或者使用getComponent方法
-    //onUpdate(): void {}
+    onStart(): void {}
 
-    //每帧更新时执行，在update之后执行，尽量不要在这里写大循环逻辑或者使用getComponent方法
-    //onLateUpdate(): void {}
-
-    //鼠标点击后执行。与交互相关的还有onMouseDown等十多个函数，具体请参阅文档。
-    //onMouseClick(): void {}
+    onUpdate(): void {}
 }
